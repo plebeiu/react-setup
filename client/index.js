be0daf42dc2bccc40355715ec516a71a0db7dee2
Jaime Lee <weput@outlook.com>
2017-03-31 10:16:06 -0500

    added dependencies and set redux store.

--- client/index.js
+++ client/index.js
@@ -1,5 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
+import {createStore, applyMiddleware, compose} from 'redux';
+import {Provider} from 'react-redux';
+import thunk from 'redux-thunk';
+import reducer from './reducer';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+const store = createStore(reducer, compose(
+    applyMiddleware(thunk),
+    /* dev tools code*/
+    window.devToolsExtension ? window.devToolsExtension():f=>f
+));
+
+
+ReactDOM.render(
+     <Provider store={store}>
+     <App />
+     </Provider>,
+document.getElementById('root')
+ )
